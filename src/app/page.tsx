cf14fe6f0a0184010baf7f446fea9cb782704fc4
srshah27 <snehilshah27@gmail.com>
2024-01-25 02:59:07 +0530

    Install UI Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import db from '@/lib/db'
+import { Button } from '@/components/ui/button'
 
 export default async function Home() {
 	const [results] = await db.query('SELECT * FROM products', ['Page', 45])
   console.log(results)
 	return (
-		<main>
+		<main className='bg-white'>
 			<h1 className='text-2xl font-abeezee'>Products</h1>
+			<Button>lmao</Button>
 		</main>
 	)
 }
