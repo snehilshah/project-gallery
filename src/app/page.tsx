93b8e47e15430d4fa761666fface9c9b0a317f5e
srshah27 <snehilshah27@gmail.com>
2024-01-30 04:46:32 +0530

    Cleaning old mess

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default async function Home() {
 	let res = JSON.stringify(results)
 	return (
 		<main className="bg-white">
+			<h1>Cleaning</h1>
 			<h1>{res}</h1>
 			<h1 className="text-8xl text-center uppercase">Project Of The Day</h1>
 		</main>
