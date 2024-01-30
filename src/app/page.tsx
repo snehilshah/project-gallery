93706f07796a0e13f7668f70147d0b8759368655
srshah27 <snehilshah27@gmail.com>
2024-01-30 14:43:02 +0530

    Remove mysql2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
-import db from '@/lib/db'
-
 export default async function Home() {
-	const [results] = await db.query('SELECT * FROM products', ['Page', 45])
-	console.log(results)
-	let res = JSON.stringify(results)
 	return (
 		<main className="bg-white">
-			<h1>{res}</h1>
 			<h1 className="text-8xl text-center uppercase">Project Of The Day</h1>
 		</main>
 	)
