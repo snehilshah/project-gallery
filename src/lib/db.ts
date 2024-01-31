7893767271e47aba8a33337dcde0d471e2a986da
srshah27 <snehilshah27@gmail.com>
2024-01-31 13:13:27 +0530

    Finally

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,13 +2,13 @@ import mysql from 'mysql2'
 
 const pool = mysql.createPool({
 	host: 'aws.connect.psdb.cloud',
-	user: 'pm0nm6boglwdxfqi2qez',
+	user: process.env.DB_USERNAME,
 	database: 'project-gallery',
 	password: process.env.DB_PASSWORD,
 	waitForConnections: true,
 	connectionLimit: 10,
-	maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-	idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+	maxIdle: 10, 					// max idle connections, the default value is the same as `connectionLimit`
+	idleTimeout: 60000, 	// idle connections timeout, in milliseconds, the default value 60000
 	queueLimit: 0,
 	enableKeepAlive: true,
 	keepAliveInitialDelay: 0,
