d1228437a6f3dab6309037f1e448f8fd282c8e56
srshah27 <snehilshah27@gmail.com>
2024-02-14 00:47:53 +0530

    add link to icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,19 +3,20 @@ import { Input } from '@/components/ui/input'
 import Image from 'next/image'
 import { ThemeButton } from './theme-provider'
 import { LoginDropDown } from './auth/LoginDropDown'
+import Link from 'next/link'
 
 export default function Navbar() {
-
-	
 	return (
 		<div className="mx-auto flex">
-			<Image
-				className="mx-4"
-				src="/star-svg.svg"
-				alt="project gallery logo"
-				width={60}
-				height={5}
-			/>
+			<Link href={'/'}>
+				<Image
+					className="mx-4"
+					src="/star-svg.svg"
+					alt="project gallery logo"
+					width={60}
+					height={5}
+				/>
+			</Link>
 			<div className="flex items-center py-4 gap-10 justify-around w-full">
 				<Input type="text" placeholder="Search" className="w-2/6" />
 				<ThemeButton />
