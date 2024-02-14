95f3dd9fc2df4a6fe4389c53319746aea0e506b9
srshah27 <snehilshah27@gmail.com>
2024-02-15 02:14:40 +0530

    remove toast

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,8 @@
 import { useToast } from '@/components/ui/use-toast'
 import { Button } from '@/components/ui/button'
 export default function Home() {
-	const { toast } = useToast()
 	return (
 		<main className="bg-white">
-			<Button
-				variant="outline"
-				onClick={() => {
-					toast({
-						variant: 'destructive',
-						title: 'Uh oh! Something went wrong.',
-						description: 'There was a problem with your request.',
-					})
-				}}
-			>
-				Show Toast
-			</Button>
 			<h1 className="text-8xl text-center uppercase">Project Of The Day</h1>
 		</main>
 	)
