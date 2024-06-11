a63f7a11cd46cbec68d967644801d6a7e934a2f9
srshah27 <snehilshah27@gmail.com>
2024-06-12 00:47:31 +0530

    add option to exclude user_id

--- app/api/(user)/user/route.ts
+++ app/api/(user)/user/route.ts
@@ -4,10 +4,12 @@ import db from '@/db/db';
 export async function GET(req: NextRequest) {
   const serchParams = req.nextUrl.searchParams;
   const query = serchParams.get('q')?.toLowerCase();
+  let exclude: number = parseInt(serchParams.get('exclude') as string);
+  if (!exclude) exclude = 0;
 
-  const [results, _] = await db.execute(
-    'SELECT user_id, user_name, name, email  FROM `users` where email like ? or user_name like ? or name like ?;',
-    ['%' + query + '%', '%' + query + '%', '%' + query + '%']
+  const [results] = await db.query(
+    'SELECT user_id, user_name, name, email  FROM `users` where ( email like ? or user_name like ? or name like ? ) and user_id not in ( ? );',
+    ['%' + query + '%', '%' + query + '%', '%' + query + '%', exclude]
   );
 
   return NextResponse.json(results);
